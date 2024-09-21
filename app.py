84fd3ecf199ba757765f9dd54c71cc3d1d8e9c00
Sathvika-Mettu <mettusathvika363@gmail.com>
2024-09-21 17:14:48 +0000

    Initial commit for Flask Pet App

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask,render_template,redirect,url_for
-from models import db,Pet
+from flask import Flask, render_template, redirect, url_for
+from models import db, Pet
 from forms import PetForm
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///pets.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pets.db'
 app.config['SECRET_KEY'] = 'your_secret_key'
+
 db.init_app(app)
-@app.route('/',methods = ['GET','POST'])
+
+# Create the database and tables
+with app.app_context():
+    db.create_all()  # Ensure this line is present
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  form = PetForm()
-  if from.validate_on_submit():
-     pet = Pet(name=form.name.data, age=form.age.data, type=form.type.data)
-     db.session.add(pet)
-     db.session.commit()
-     return redirect(url_for('index'))
-  if__name__=='__main__':
+    form = PetForm()
+    if form.validate_on_submit():
+        pet = Pet(name=form.name.data, age=form.age.data, type=form.type.data)
+        db.session.add(pet)
+        db.session.commit()
+        return redirect(url_for('index'))
+    pets = Pet.query.all()
+    return render_template('view_pets.html', form=form, pets=pets)
+
+if __name__ == '__main__':
     app.run(debug=True)
+

a746e5fe5be5a71c7f488732d629fbd4050c11f9
Sathvika-Mettu <mettusathvika363@gmail.com>
2024-10-23 23:27:42 -0400

    Update forms.py

--- forms.py
+++ forms.py
@@ -5,5 +5,5 @@ from wtforms.validators import DataRequired
 class PetForm(FlaskForm):
     name = StringField('Pet Name', validators=[DataRequired()])
     age = IntegerField('Pet Age', validators=[DataRequired()])
-    type = StringField('Pet Type', validators=[DataRequired()])
+    pet_type = StringField('Pet Type', validators=[DataRequired()])
     submit = SubmitField('Add Pet')

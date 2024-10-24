e631e880985d208b70c6f3ea92ec3c07c87893bb
Sathvika-Mettu <mettusathvika363@gmail.com>
2024-10-23 23:28:07 -0400

    Update models.py

--- models.py
+++ models.py
@@ -6,4 +6,4 @@ class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     age = db.Column(db.Integer, nullable=False)
-    type = db.Column(db.String(100), nullable=False)
+    pet_type = db.Column(db.String(100), nullable=False)
